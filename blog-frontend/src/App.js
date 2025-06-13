6681e671eb3032c1dbe83af1bd2dd897e2848082
Aaryan Pradhan <pradhanaaryan@gmail.com>
2025-06-13 09:57:47 +0530

    frontend

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-2xl text-center mt-10 font-bold text-blue-500">
+      Blog Platform Starter 🚀
+    </div>
+  );
+}
+
+export default App;
